12fd8a4f0f6964847fe03d65c04f0c7ad3e7eb6b
Anya Tamara <anyatmr@gmail.com>
2021-06-23 11:22:00 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import QRCode from 'react-qr-code';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <HandleQrInput></HandleQrInput>
       </header>
     </div>
   );
 }
-
 export default App;
+
+class HandleQrInput extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state= { value :'', randomInt: 0} 
+    this.handleChange = this.handleChange.bind(this)
+  }
+ 
+  handleChange(event) {
+    {/* We Can Generate Random Number  and change the state here */}
+    this.setState({randomInt: Math.floor(Math.random() *10000000000000)}) 
+    this.setState({value: event.target.value});
+  }
+  render() {
+    return (
+      <div style={{display:"flex"}, {flexDirection:"column"}}>
+        <h1>The QR will say: {this.state.value}</h1>
+        <h1>{this.state.randomInt}</h1>
+        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+        <QRCode value={this.state.value} />
+      </div>
+      
+    )
+  }
+}
+
+
